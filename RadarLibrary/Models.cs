27880a97f273f3e4caa9724fa4f04d01dd5d7600
lovro <48408654+7loki7@users.noreply.github.com>
2024-11-19 21:46:37 +0100

    Add project files.

---
+++ RadarLibrary/Models.cs
@@ -0,0 +1,22 @@
+﻿namespace RadarLibrary
+{
+    public class Models
+    {
+        public class RadarResponse
+        {
+            public List<RadarTarget>? Targets { get; set; }
+            public long Timestamp { get; set; }
+            public bool IsOnline { get; set; }
+            public string? LastError { get; set; }
+        }
+
+        public class RadarTarget
+        {
+            public int Id { get; set; }
+            public double Distance { get; set; }
+            public double Angle { get; set; }
+            public double Speed { get; set; }
+            public double Strenght { get; set; }
+        }
+    }
+}
---
+++ RadarLibrary/RadarLib.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using static RadarLibrary.Models;
+
+namespace RadarLibrary
+{
+    public class RadarCommunicator(string radarIp)
+    {
+        private readonly HttpClient _client = new();
+
+        public async Task<RadarResponse> GetRadarStatusAsync()
+        {
+            try
+            {
+                var response = await _client.GetAsync(radarIp);
+                if (response.IsSuccessStatusCode)
+                {
+                    return new RadarResponse { IsOnline = true };
+                }
+                return new RadarResponse { IsOnline = false};
+            }
+            catch (Exception ex)
+            {
+                return new RadarResponse { IsOnline = false, LastError = ex.Message };
+            }
+        }
+
+        public async Task<List<RadarTarget>?> GetRadarDataAsync()
+        {
+            try
+            {
+                var response = await _client.GetAsync(radarIp);
+                response.EnsureSuccessStatusCode();
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+                if (string.IsNullOrEmpty(responseContent))
+                {
+                    Console.WriteLine($"Received empty response");
+                    return null;
+                }
+
+                var radarResponse = JsonConvert.DeserializeObject<RadarResponse>(responseContent);
+                return radarResponse?.Targets ?? null;
+            }
+            catch (HttpRequestException httpEx)
+            {
+                Console.WriteLine($"HTTP Request Error: {httpEx.Message}");
+                return null;
+            }
+            catch (JsonException jsonEx)
+            {
+                Console.WriteLine($"JSON Deserialization Error: {jsonEx.Message}");
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected Error: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
